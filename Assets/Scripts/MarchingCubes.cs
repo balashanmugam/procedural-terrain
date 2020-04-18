509bd67a4265896a2cf1306954fc70c329519422
Bala Shanmugam <balashanmugam.1996@gmail.com>
2020-04-18 02:06:58 -0700

    final refactor

---
+++ Assets/Scripts/MarchingCubes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarchingCubes : MonoBehaviour {
+    
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<int> tris = new List<int>();
+    
+    public List<Vector3> Vertices
+    {
+        get => vertices;
+        set => vertices = value;
+    }
+
+    public List<int> Tris
+    {
+        get => tris;
+        set => tris = value;
+    }
+
+    public void ClearMeshData() {
+        vertices.Clear();
+        tris.Clear();
+    }
+
+    public void MarchCube(Vector3 pos, float[] cube, float ts) {
+        int index = marchingcubes.Utils.CalculateCubeIndex(cube,ts);
+        
+        if (index == 0 || index == 255)
+            return;
+
+        int e = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                int indice = marchingcubes.Utils.TriangleTable[index, e];
+
+                if (indice == -1) return;
+
+                Vector3 vertice1 = pos + marchingcubes.Utils.EdgeTable[indice, 0];
+                Vector3 vertice2 = pos + marchingcubes.Utils.EdgeTable[indice, 1];
+                
+                vertices.Add(vertice1 + vertice2 /
+                             2.0f);
+                tris.Add(vertices.Count - 1);
+                e++;
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+
+ [RequireComponent(typeof(MarchingCubes))]
+ public class TerrainGenerator : MonoBehaviour {
+     private MarchingCubes mc;
+     
+     [SerializeField] private int width = 32;
+     [SerializeField] private int height = 8;
+     [SerializeField] private float terrainSurface = 0.5f;
+     [SerializeField] private float[,,] tmap;
+     
+     private MeshFilter meshFilter;
+     private void Start() {
+         mc = GetComponent<MarchingCubes>();
+         meshFilter = GetComponent<MeshFilter>();
+         mc.ClearMeshData();
+         
+         tmap = GenerateTerrain();
+         CreateMeshData();
+         meshFilter.mesh = marchingcubes.Utils.BuildMesh(mc.Vertices,mc.Tris);
+     }
+
+     private float[,,] GenerateTerrain() {
+         float[,,] map = new float[width + 1,height + 1, width + 1];
+         for (int x = 0; x < width; x++) {
+             for (int y = 0; y < height; y++) {
+                 for (int z = 0; z < width; z++) {
+                     float h = (float) height *
+                               Mathf.PerlinNoise((float) x / 16f * 1.5f + 0.001f, (float) z / 16f * 1.5f + 0.001f);
+                     float point = 0;
+ 
+                     if (y <= (h - 0.5f)) point = 0f;
+                     else if (y > h + 0.5f) point = 1f;
+                     else if (y > h) point = (float) y - h;
+                     else point = h - (float) y;
+ 
+                     map[x, y, z] = point;
+                 }
+             }
+         }
+         return map;
+     }
+     
+     private void CreateMeshData() {
+         for (int i = 0; i < width; i++) {
+             for (int j = 0; j < height; j++) {
+                 for (int k = 0; k < width; k++) {
+                     float[] cube = new float[8];
+
+                     for (int l = 0; l < 8; l++) {
+                         Vector3Int corner = new Vector3Int(i, j, k) + marchingcubes.Utils.CornerTable[l];
+                         cube[l] = tmap[corner.x, corner.y, corner.z];
+                     }
+                     mc.MarchCube(new Vector3(i, j, k), cube,terrainSurface);
+                 }
+             }
+         }
+     }
+ }
